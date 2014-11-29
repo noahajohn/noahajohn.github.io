4754fd9d9d782b639e918d46803f33d7d68bc6d7
Noah John <noah.a.john@gmail.com>
2014-11-28 19:21:42 -0700

    Ported php includes

--- calcsee3d/manual_chapter_nav.php
+++ calcsee3d/manual_chapter_nav.php
@@ -1,7 +1,7 @@
 <ul>
-		<li><a href="manualchapter1.php">Chapter 1: Introduction</a></li>
-		<li><a href="manualchapter2.php">Chapter 2: Traces Visualization</a></li>
-		<li><a href="manualchapter3.php">Chapter 3: Tangent Plane Visualization</a></li>
-		<li><a href="manualchapter4.php">Chapter 4: Limit Path Visualization</a></li>
-		<li><a href="manualchapter5.php">Chapter 5: Suggested Uses of Visualizations</a></li>
+		<li><a href="manualchapter1.html">Chapter 1: Introduction</a></li>
+		<li><a href="manualchapter2.html">Chapter 2: Traces Visualization</a></li>
+		<li><a href="manualchapter3.html">Chapter 3: Tangent Plane Visualization</a></li>
+		<li><a href="manualchapter4.html">Chapter 4: Limit Path Visualization</a></li>
+		<li><a href="manualchapter5.html">Chapter 5: Suggested Uses of Visualizations</a></li>
 </ul>
